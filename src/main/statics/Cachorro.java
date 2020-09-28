83c50d0e7273e50104621c58743b56d8fb967025
Pleiterson Amorim <pleiterson@gmail.com>
2020-09-28 18:09:23 -0300

    modificadores de acessos estaticos (static)

---
+++ src/main/statics/Cachorro.java
@@ -0,0 +1,10 @@
+package one.digitalinnovation.statics;
+
+public class Cachorro {
+    //public String zoologia = "Quadrupede"; // uma instância
+    public static String zoologia = "Quadrupede"; // todas instâncias
+
+    public static String late() {
+        return "Au!Au!";
+    }
+}
